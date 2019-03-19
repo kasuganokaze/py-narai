9fd2d025ca9bf44814797095e4330ed6196fe18c
kaze <957786753@qq.com>
2019-03-19 10:57:06 +0800

    一些改动

--- venv/test/selenium_test.py
+++ venv/test/selenium_test.py
@@ -6,8 +6,7 @@ prefs = {"profile.managed_default_content_settings.images": 2}
 chrome_options.add_experimental_option("prefs", prefs)
 
 driver = webdriver.Chrome(executable_path="/Users/kaze/tool/chromedriver", chrome_options=chrome_options)
-prefs = {"profile.managed_default_content_settings.images": 2}
-driver.get('https://www.qq.com/')
-tags = driver.find_elements_by_tag_name("a")
+driver.get('https://detail.tmall.com/item.htm?spm=a220o.1000855.0.da321h.17f32a4fJzPKPF&id=579794586729&sku_properties=10004:709990523;5919063:6536025')
+tags = driver.find_elements_by_class_name("tm-sale-prop")
 for tag in tags:
-    print(tag)
+    print(tag.text)
--- venv/test/web_test.py
+++ venv/test/web_test.py
@@ -2,15 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = "https://tieba.baidu.com/f?kw=%E9%9B%80%E9%AD%82&fr=index&fp=0&ie=utf-8"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup.prettify())
-ass = soup.find_all('a', class_='j_th_tit')
-for a in ass:
-    print("https://tieba.baidu.com" + a['href'])
+# url = "https://tieba.baidu.com/f?kw=%E9%9B%80%E9%AD%82&fr=index&fp=0&ie=utf-8"
+# page = requests.get(url)
+# soup = BeautifulSoup(page.content, 'html.parser')
+# # print(soup.prettify())
+# ass = soup.find_all('a', class_='j_th_tit')
+# for a in ass:
+#     print("https://tieba.baidu.com" + a['href'])
+#
+# url = 'https://h5api.m.taobao.com/h5/mtop.taobao.detail.getdetail/6.0/?sign=&data=%7B%22itemNumId%22%3A%22577284665556%22%7D%27'
+# page = requests.get(url)
+# result_json = json.loads(page.text)
+# print(result_json['data'])
 
-url = 'https://h5api.m.taobao.com/h5/mtop.taobao.detail.getdetail/6.0/?sign=&data=%7B%22itemNumId%22%3A%22577284665556%22%7D%27'
+
+url = 'https://detail.tmall.com/item.htm?id=579794586729&skuId=3856455121135'
 page = requests.get(url)
-result_json = json.loads(page.text)
-print(result_json['data'])
+print(page.text)
+soup = BeautifulSoup(page.content, 'html.parser')
+ass = soup.find_all('section', class_='config-list')
+for a in ass:
+    pass
+
